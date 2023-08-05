f564335bc26d24ea889830bf1d59834082eb801c
CN\Mace Y Li <mace.y.li@cn.pwc.com>
2023-08-05 21:30:22 +0800

    修改 级联组件 loading 全遮罩

--- src/packages/elementElCascader/elementElCascader.vue
+++ src/packages/elementElCascader/elementElCascader.vue
@@ -23,6 +23,10 @@
           firstClickNode:{
             type:Boolean,
             default:false
+          },
+          loading:{
+            type:Boolean,
+            default:false
           }
         },
         mounted(){
@@ -39,13 +43,70 @@
           if(this.firstClickNode){
             _vnode.componentInstance.$el.querySelector(".el-cascader__dropdown").addEventListener("click",this.firstNodeFunc,true)
           }
+
+          //保存对象
+          this.$dropdownElem = _vnode.componentInstance.$el.querySelector(".el-cascader__dropdown");
+          this._createPlaceholder();
         },
         methods:{
+          _createPlaceholder(){
+            let _d = document.createElement("div"),
+            _style = "width:0px;height:0px;position:relative",
+            _dId = `__d__${this._uid}`,
+            _dropId = `__dropd__${this._uid}`;
+
+            _d.id = _dId
+            _d.style = _style;
+            this.$el.appendChild(_d);
+
+            let _dropd = document.createElement("div");
+            _dropd.id = _dropId
+            _dropd.style = _style;
+
+            //保存在下级对象里
+            this._dId = _dId;
+            this._dropId = _dropId;
+
+            this.$el.querySelector(".el-cascader__dropdown").appendChild(_dropd);
+
+
+            function loadPlace(){
+              if(!document.querySelector(`#${_dId}`)){
+                setTimeout(()=>{
+                  console.log('settimeout')
+                  loadPlace();
+                },100)
+              }else{
+                this._createLoadingPlace(_dId);
+                this._createLoadingPlace(_dropId);
+              }
+            }
+            
+
+            loadPlace.call(this);
+           
+          },
+          _createLoadingPlace(_id){
+
+            let mask = document.createElement("div"),
+            loading = document.createElement("div"),
+            id = `#${_id}`
+
+            mask.id = `_mask_${_id}`;
+            loading.id = `_loading_${_id}`;
+
+            mask.className = '__transparentbg__'
+            loading.className = '__loadingloader__'
+
+            document.querySelector(id).appendChild(mask);
+            document.querySelector(id).appendChild(loading);
+          },
           firstNodeFunc(event){
               let checkboxValue = void 0,
               _node = void 0
               if(event.target.nodeName === 'SPAN' && event.target.className.indexOf('el-checkbox') > -1){
-                checkboxValue = event.target.parentNode.parentNode.__vue__.selfModel;
+                //checkboxValue = event.target.parentNode.parentNode.__vue__.selfModel;
+                checkboxValue = event.target.parentNode.className.indexOf('is-checked') > -1 ? false : true;
                 _node = event.target.parentNode.parentNode.parentNode.__vue__.node;
                  //console.log(checkboxValue,_node)
               }
@@ -84,9 +145,87 @@
               }
           }
         },
+        watch:{
+          loading:{
+            handler(v,ov){
+              if(v === ov){
+                return;
+              }
+
+              setTimeout(()=>{
+                let _dIdEl = document.querySelector(`#${this._dId}`),
+                _dropIdEl = document.querySelector(`#${this._dropId}`),
+                _dIdWidth = 0,_dIdHeight = 0,
+                _dropIdWidth = 0,_dropIdHeight = 0;
+
+                if(!!v){
+                    if(!!_dIdEl){
+                      let {width,height} = this.$el.getBoundingClientRect();
+                      _dIdWidth = width;
+                      _dIdHeight = height;
+                    }
+
+                    if(!!_dropIdEl){
+                      let {width,height} = this.$dropdownElem.getBoundingClientRect();
+                      _dropIdWidth = width;
+                      _dropIdHeight = height;
+                    }
+                } 
+                
+
+                if(!!_dIdEl){
+                    _dIdEl.querySelector(`#_mask_${this._dId}`).setAttribute("style", 
+                    `
+                    width:${_dIdWidth}px;height:${_dIdHeight}px;position:absolute;left:0px;top:-${_dIdHeight}px;z-index:100000;
+                    display:${!!v ? 'block' : 'none'}
+                    `)
+
+                     _dIdEl.querySelector(`#_loading_${this._dId}`).setAttribute("style", 
+                     `
+                    position:absolute;left:${_dIdWidth/2}px;top:-${_dIdHeight/1.5}px;z-index:100000;
+                     display:${!!v ? 'block' : 'none'}
+                     `)
+                }
+
+                if(!!_dropIdEl){
+                  _dropIdEl.querySelector(`#_mask_${this._dropId}`).setAttribute("style", `
+                  width:${_dropIdWidth}px;height:${_dropIdHeight}px;position:absolute;left:0px;top:-${_dropIdHeight}px;z-index:100000;
+                  display:${!!v ? 'block' : 'none'}`)
+
+                   _dropIdEl.querySelector(`#_loading_${this._dropId}`).setAttribute("style", 
+                   `
+                   position:absolute;left:${_dropIdWidth/2}px;top:-${_dropIdHeight/2}px;z-index:100000;
+                   display:${!!v ? 'block' : 'none'}
+                   `);
+                   
+                }
+              },100)
+            },
+            immediate:true
+          }
+        },
         render(h){
           let _vnode =  this.$slots.default[0];
           return _vnode;
         }
     }
 </script>
+<style>
+.__loadingloader__ {
+  width: 8px;
+  height: 8px;
+  border: 3px solid #ccc;
+  border-top-color: #666;
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
+}
+
+.__transparentbg__ {
+  background-color: rgba(255, 255, 255, 0.7);
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
